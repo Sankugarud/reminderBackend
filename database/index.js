c4745c66336ca0e1c9722b46b1bf5910127e1761
Sankugarud <sanketgarud1998@gmail.com>
2023-11-08 01:47:46 +0530

    edit database link

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
  async function connectDatabase(){
-    const db = await mongoose.connect('mongodb+srv://new_user:Sanket123@cluster.mongodb.net/MedicineReminder')
+    const db = await mongoose.connect('mongodb+srv://new_user:Sanket123@cluster0.pnpsdhv.mongodb.net/?retryWrites=true&w=majority')
     return db;
  }  
 
